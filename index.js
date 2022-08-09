d62c1c0f3d700a49cc1a1c2e7bd37fe8fda3e0f1
Andrew Virts <asvirts@outlook.com>
2022-08-09 12:49:12 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let myReads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    myReads.push(inputEl.value)
+    console.log(myLeads)
+})
